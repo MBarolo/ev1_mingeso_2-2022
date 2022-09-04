e24541515ddf3aa92634bcd9cb89306ae87607ba
MBarolo <73084603+MBarolo@users.noreply.github.com>
2022-09-04 11:56:47 -0300

    Proyecto creado

---
+++ ev1_mingeso_MueblesStgo/src/main/java/com/muebles/ev1/Ev1MingesoMueblesStgoApplication.java
@@ -0,0 +1,13 @@
+package com.muebles.ev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ev1MingesoMueblesStgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ev1MingesoMueblesStgoApplication.class, args);
+	}
+
+}
