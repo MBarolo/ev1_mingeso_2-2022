5a961ce9e42ff83cde51b9738a53019d71397e5d
Matías Barolo <matias.barolo@usach.cl>
2022-10-02 17:28:00 -0300

    Varios fixes, Test para servicio planillas realizado

--- sql/load.sql
+++ sql/load.sql
@@ -1,14 +1,17 @@
 INSERT INTO empleados (rut, apellidos, nombres, categoria, fecha_ingreso, fecha_nac)
-values('11.234.123-6', 'Emp1', 'emp1', 'a', '2000-01-01', '2000-01-01');
+values('11.234.123-6', 'Nombre_1', 'Apellido_1', 'a', '2000-01-01', '2000-01-01');
 
 INSERT INTO empleados (rut, apellidos, nombres, categoria, fecha_ingreso, fecha_nac)
-values('12.457.562-3', 'Emp2', 'emp2', 'a', '2000-01-01', '2000-01-01');
+values('12.457.562-3', 'Nombre_2', 'Apellido_2', 'b', '2000-01-01', '2000-01-01');
 
 INSERT INTO empleados (rut, apellidos, nombres, categoria, fecha_ingreso, fecha_nac)
-values('21.142.354-k', 'Emp3', 'emp3', 'a', '2000-01-01', '2000-01-01');
+values('21.142.354-k', 'Nombre_3', 'Apellido_3', 'c', '2000-01-01', '2000-01-01');
 
 INSERT INTO empleados (rut, apellidos, nombres, categoria, fecha_ingreso, fecha_nac)
-values('17.765.876-2', 'Emp2', 'emp2', 'a', '2000-01-01', '2000-01-01');
+values('17.765.876-2', 'Nombre_4', 'Apellido_4', 'a', '2000-01-01', '2000-01-01');
 
-INSERT INTO marcas(fecha, hora, rut_empleado, justificativo, autorizacion)
-values ("2000-01-01","00:00:00", "17.765.876-2", false, false);
+INSERT INTO empleados (rut, apellidos, nombres, categoria, fecha_ingreso, fecha_nac)
+values('20.457.671-9', 'Nombre_5', 'Apellido_5', 'a', '2000-01-01', '2000-01-01');
+
+INSERT INTO empleados (rut, apellidos, nombres, categoria, fecha_ingreso, fecha_nac)
+values('27.752.982-4', 'Nombre_6', 'Apellido_6', 'b', '2000-01-01', '2000-01-01');
